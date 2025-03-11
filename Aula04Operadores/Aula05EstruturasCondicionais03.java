d1038a928e4c756a520627169c8d3402a30d1aa6
gabrfrade01 <gabr.alex01@hotmail.com>
2025-03-11 20:39:08 -0300

    Feat: adiciando novos recursos ref. a Operador Ternário

--- Aula04Operadores/Aula05EstruturasCondicionais03.java
+++ Aula04Operadores/Aula05EstruturasCondicionais03.java
@@ -1,19 +1,27 @@
 package Aula04Operadores;
 
+import java.sql.SQLOutput;
+
 public class Aula05EstruturasCondicionais03 {
     public static void main(String[] args) {
 // OPERADORES TERNÁRIOS
         // O operador ternário é uma expressão condicional que avalia uma condição e retorna um valor.
+
+        //(condição) ? verdadeiro : falso
         double salario = 6000;
         String mensagemDoar = "Eu vou doar 500 pra caridade";
         String mensagemNaoDoar = "Eu ainda não tenho condições";
-        String resultado;
-        if (salario >= 5000) {
-            resultado = mensagemDoar;
-        }
-        else{
-            resultado = mensagemNaoDoar;
-        }
+        String resultado = salario > 5000 ? mensagemDoar : mensagemNaoDoar; //sempre pega uma String, mensagemDoar o resulta é uma String...
+
         System.out.println(resultado);
+// OU PODERIA FICAR DESSA FORMA:
+
+
+        double salario02 = 17000;
+        String resultado02 = salario02 > 500 ? "Eu vou dar um presente pra Bela" : "Eu não posso dar o presente ainda";
+        System.out.println(resultado02); //mais simplificado ainda!
+
+
     }
+
 }
