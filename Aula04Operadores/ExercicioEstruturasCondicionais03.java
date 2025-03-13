e26ac9c3e87d70ccb98e3741833878fb35d886e2
gabrfrade01 <gabr.alex01@hotmail.com>
2025-03-12 21:50:11 -0300

    Feat: Exercício 03

---
+++ Aula04Operadores/ExercicioEstruturasCondicionais03.java
@@ -0,0 +1,19 @@
+package Aula04Operadores;
+
+public class ExercicioEstruturasCondicionais03 {
+    public static void main(String[] args) {
+        double salarioAnual = 70000;
+        double primeiraFaixa = 9.70 / 100;
+        double segundaFaixa = 37.35 / 100;
+        double terceiraFaixa = 49.50 / 100;
+
+        if (salarioAnual <= 34712) {
+            salarioAnual *= primeiraFaixa;
+        } else if (salarioAnual >= 34713 && salarioAnual <= 68507) {
+            salarioAnual *= segundaFaixa;
+        } else {
+            salarioAnual *= terceiraFaixa;
+        }
+        System.out.println(salarioAnual);
+    }
+}
